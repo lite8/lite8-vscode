91de7e9957404620ecae0ccfb7e32cd267ccb2da
x2nie <x2nie@yahoo.com>
2021-03-11 19:35:00 +0700

    far better. it may need restructure for future flexiblity + consistent. current color for __section__ is inconsistent

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(
         {language: "lite8"}, new Lite8DocumentSymbolProvider()
     ));
+    
     // context.subscriptions.push(vscode.languages.registerFoldingRangeProvider(
     //     {language: "lite8"}, new Lite8FoldingProvider()
     // ));
@@ -20,19 +21,59 @@ class Lite8DocumentSymbolProvider implements vscode.DocumentSymbolProvider {
     public provideDocumentSymbols(document: vscode.TextDocument,
             token: vscode.CancellationToken): Promise<vscode.DocumentSymbol[]> {
         return new Promise((resolve, reject) => {
+            const txt = document.getText();
+            const regex = /(__\w+__)/gm;
+            let m,i;
+            let _symbols = [];
+            while ((m = regex.exec(txt)) !== null) {
+                // This is necessary to avoid infinite loops with zero-width matches
+                if (m.index === regex.lastIndex) {
+                    regex.lastIndex++;
+                }
+                
+                _symbols.push({
+                    name: m[0],
+                    start: m.index,
+                    finish: 0
+                })
+                // The result can be accessed through the `m`-variable.
+                // m.forEach((match, groupIndex) => {
+                //     console.log(`Found match, group ${groupIndex}: ${match}`);
+                // });
+            }
+            for(i=0;i<_symbols.length-1;i++){
+                _symbols[i].finish = _symbols[i+1].start-1;
+            }
+            _symbols[_symbols.length-1].finish = txt.length-1;
+
+            let symbols: vscode.DocumentSymbol[] = [];
+            for (i = 0; i < _symbols.length; i++) {
+                var sym = _symbols[i];
+                var r = new vscode.Range( document.positionAt(sym.start), document.positionAt(sym.finish) );
+                let marker_symbol = new vscode.DocumentSymbol(
+                    //this.format(tokens[0]) + " " + tokens[1],
+                    sym.name,
+                    'Component',
+                    vscode.SymbolKind.Enum,
+                    r,r
+                )
+                symbols.push(marker_symbol);
+            }
+            resolve(symbols);
+        });
+    }
+    /* private _quickSymbols(txt: string) {
+
+    }
+    public _old_provideDocumentSymbols(document: vscode.TextDocument,
+            token: vscode.CancellationToken): Promise<vscode.DocumentSymbol[]> {
+        return new Promise((resolve, reject) => {
             // var symbols = [];
             let symbols: vscode.DocumentSymbol[] = [];
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 if (line.text.startsWith("__")) {
-                    /*
-                    symbols.push({
-                        name: line.text.substr(3, line.text.length - 6),
-                        kind: vscode.SymbolKind.Namespace,
-						location: new vscode.Location(document.uri, line.range),
-						containerName: line.text.substr(3, line.text.length - 6)
-                    })
-                    */
+                    
                     let marker_symbol = new vscode.DocumentSymbol(
                         //this.format(tokens[0]) + " " + tokens[1],
                         line.text.substr(2, line.text.length - 4),
@@ -44,12 +85,15 @@ class Lite8DocumentSymbolProvider implements vscode.DocumentSymbolProvider {
             }
             resolve(symbols);
         });
-    }
+    } */
 }
 
 class Lite8FoldingProvider implements vscode.FoldingRangeProvider {
-    public provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext,
-            token: vscode.CancellationToken): vscode.ProviderResult<vscode.FoldingRange[]> {
+    async provideFoldingRanges(
+        document: vscode.TextDocument, 
+        context: vscode.FoldingContext,
+        token: vscode.CancellationToken
+    ): Promise<vscode.FoldingRange[] | undefined> {
         return new Promise((resolve, reject) => {
             var ranges = [];
             var prev = -1;
@@ -76,6 +120,35 @@ class Lite8FoldingProvider implements vscode.FoldingRangeProvider {
     }
     
 }
+// class Lite8FoldingProvider implements vscode.FoldingRangeProvider {
+//     public provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext,
+//             token: vscode.CancellationToken): vscode.ProviderResult<vscode.FoldingRange[]> {
+//         return new Promise((resolve, reject) => {
+//             var ranges = [];
+//             var prev = -1;
+//             for (var i = 0; i < document.lineCount; i++) {
+//                 var line = document.lineAt(i);
+//                 if (line.text.startsWith("__")) {
+//                     if (prev >= 0) {
+//                         ranges.push({
+//                             start: prev,
+//                             end: i - 1,
+//                             kind: vscode.FoldingRangeKind.Region
+//                         });
+//                     }
+//                     prev = i;
+//                 }
+//             }
+//             ranges.push({
+//                 start: prev,
+//                 end: document.lineCount - 1,
+//                 kind: vscode.FoldingRangeKind.Region
+//             });
+//             resolve(ranges);
+//         });
+//     }
+    
+// }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
