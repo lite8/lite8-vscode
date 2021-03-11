565de9854d54d6b82bd6a7b8c6d2bd24469b1181
x2nie <x2nie@yahoo.com>
2021-03-11 08:00:14 +0700

    initial run

---
+++ src/extension.ts
@@ -0,0 +1,68 @@
+import * as vscode from 'vscode';
+
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(
+        {language: "lite8"}, new Lite8DocumentSymbolProvider()
+    ));
+    context.subscriptions.push(vscode.languages.registerFoldingRangeProvider(
+        {language: "lite8"}, new Lite8FoldingProvider()
+    ));
+    // context.subscriptions.push(vscode.languages.registerHoverProvider(
+    //     {language: "lite8"}, new Lite8HoverProvider()
+    // ));    
+}
+
+class Lite8DocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+    public provideDocumentSymbols(document: vscode.TextDocument,
+            token: vscode.CancellationToken): Thenable<vscode.SymbolInformation[]> {
+        return new Promise((resolve, reject) => {
+            var symbols = [];
+            for (var i = 0; i < document.lineCount; i++) {
+                var line = document.lineAt(i);
+                if (line.text.startsWith("___")) {
+                    symbols.push({
+                        name: line.text.substr(3, line.text.length - 6),
+                        kind: vscode.SymbolKind.Namespace,
+						location: new vscode.Location(document.uri, line.range),
+						containerName: line.text.substr(3, line.text.length - 6)
+                    })
+                }
+            }
+            resolve(symbols);
+        });
+    }
+}
+
+class Lite8FoldingProvider implements vscode.FoldingRangeProvider {
+    public provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext,
+            token: vscode.CancellationToken): vscode.ProviderResult<vscode.FoldingRange[]> {
+        return new Promise((resolve, reject) => {
+            var ranges = [];
+            var prev = -1;
+            for (var i = 0; i < document.lineCount; i++) {
+                var line = document.lineAt(i);
+                if (line.text.startsWith("___")) {
+                    if (prev >= 0) {
+                        ranges.push({
+                            start: prev,
+                            end: i - 1,
+                            kind: vscode.FoldingRangeKind.Region
+                        });
+                    }
+                    prev = i;
+                }
+            }
+            ranges.push({
+                start: prev,
+                end: document.lineCount - 1,
+                kind: vscode.FoldingRangeKind.Region
+            });
+            resolve(ranges);
+        });
+    }
+    
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
